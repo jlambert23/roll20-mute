9b715b822d3ce35eeefcbb6e6dfed0e62706a939
Justin Lambert <justin.lambert@synapsestudios.com>
2024-07-01 01:20:39 -0400

    mute on hint click

--- public/script.js
+++ public/script.js
@@ -1,7 +1,32 @@
+async function getCurrentTab() {
+  const [tab] = await chrome.tabs.query({
+    active: true,
+    lastFocusedWindow: true,
+  });
+  return tab;
+}
+
+function toggleMute() {
+  const muteButton = document.querySelector(
+    "#player_-MR0WIh6DmKBdErY8nmt > div.video-container > div.player-bookmark.ui-draggable.ui-draggable-handle > div.av-controls > i:nth-child(1)",
+  );
+  muteButton.click();
+}
+
+async function onClick() {
+  const tab = await getCurrentTab();
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    func: toggleMute,
+  });
+}
+
 (async () => {
   const commands = await chrome.commands.getAll();
   const toggleCommand = commands.find(({ name }) => name === "toggle-mute");
   if (toggleCommand) {
-    document.getElementById("hint").innerText = toggleCommand.shortcut;
+    const hint = document.getElementById("hint");
+    hint.innerText = toggleCommand.shortcut;
+    hint.onclick = onClick;
   }
 })();
